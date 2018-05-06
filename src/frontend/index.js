52a8a8d6b5dfba415b9995948050d659c230378a
Matheus Saldanha <mhjsaldanha@gmail.com>
2018-05-06 12:00:30 -0300

    Fix all Codacy issues

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,14 +1,12 @@
-const path = require('path');
-const app = require('express')();
-const request = require('request');
+const path = require("path");
+const app = require("express")();
+const request = require("request");
 
-app.get('/', (req, res) => {
-	request('http://172.18.0.21:5000', function (Nerror, Nresponse, body) {
-		console.log(Nerror, Nresponse, body, '\n');
-		res.send('Hello World!<br><br>' + body);
+app.get("/", (req, res) => {
+	request("http://172.18.0.21:5000", function (Nerror, Nresponse, body) {
+		res.send("Hello World!<br><br>" + body);
 	});
 });
 
 // Begin serving
 app.listen(80);
-console.log(`Server listening on 80`);
