bdfb0aad3cf5f787e0ea4316f935ba9c2cf978dd
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-28 11:25:35 -0300

    Initialize our home page.

--- src/frontend/main-page/src/App.js
+++ src/frontend/main-page/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function navbar(){
+  return(
+    <nav className="navbar navbar-default" style={{marginBottom: 0, borderRadius: 0}}>
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="/">Entretenibit</a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="myNavbar">
+          <ul className="nav navbar-nav">
+            <li><a href="/aboutus">Sobre Nós</a></li>
+            <li><a href="/register">Cadastre-se</a></li>
+            <li><a href="https://github.com/martchellop/Entretenibit">Nosso Projeto</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {navbar()}
+        <div className="container-fluid jumbotron">
+          <h1>Entretenibit</h1>
+          <h3><small>Sua querida base de eventos.</small></h3>
+        </div>
       </div>
     );
   }
