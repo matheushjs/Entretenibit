0c7d37fbf84745da9afd596a2a45b9f5bafba54a
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-28 13:23:49 -0300

    Fix codacy issues

--- src/frontend/build.bash
+++ src/frontend/build.bash
@@ -1,1 +1,2 @@
+#!/bin/bash
 cd main-page && npm install && npm run-script build && cd ..
--- src/frontend/main-page/src/App.js
+++ src/frontend/main-page/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 function navbar(){
   return(
