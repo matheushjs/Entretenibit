e52ff165287dbc49202e342d960f2cdcf1e3cf1d
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-28 11:45:26 -0300

    Serve react app from root javascript

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,12 +1,18 @@
 const path = require("path");
-const app = require("express")();
+const express = require("express");
+const app = express();
 const request = require("request");
 
-app.get("/", (req, res) => {
-	request("http://172.18.0.21:5000", function (Nerror, Nresponse, body) {
-		res.send("Hello World!<br><br>" + body);
-	});
+/*
+app.get("/pythontest", (req, res) => {
+  console.log("ehhhh");
+  request("http://172.18.0.21:5000", function (Nerror, Nresponse, body) {
+    res.send("Hello World!<br><br>" + body);
+  });
 });
+*/
+
+app.use(express.static(path.resolve("./main-page/build/")));
 
 // Begin serving
-app.listen(80);
+app.listen(8081);
