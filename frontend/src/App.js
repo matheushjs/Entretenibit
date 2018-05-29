bac6568319b96ba654854adb051e84b042516a6b
FelSiq <felipe.siqueira@usp.br>
2018-05-28 21:29:22 -0300

    Initial main page scratch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import SearchButton from './SearchButton/SearchButton';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	<SearchBar 
+		textStyle={{
+			margin: 'auto', 
+			width: '50vw'}}
+		divStyle={{
+			lineHeight: '75vh',
+			display: 'inline', 
+			padding: '3px', 
+			margin: 'auto'}} />
+        <SearchButton 
+		buttonStyle={{
+			lineHeight: '75vh',
+			display: 'inline', 
+			padding: '3px', 
+			margin: 'auto', 
+			width: '50%'}} />
       </div>
     );
   }
