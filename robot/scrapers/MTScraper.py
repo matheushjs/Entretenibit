dedb29cb298119e8ecbde661b739f9aed6855be4
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-29 20:32:28 -0300

    Make scrapers a python module

---
+++ robot/scrapers/MTScraper.py
@@ -0,0 +1,13 @@
+from .ScraperBase import ScraperBase
+
+class MTScraper(ScraperBase):
+    """Scraper for 'Teatro Municipal de São Carlos'"""
+
+    def __init__(self):
+        pass
+
+    def scrapeEvents(self):
+        pass
+
+    def getEvents(self):
+        pass
