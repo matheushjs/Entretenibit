e99492dd85df4a3d77653293002ce31c5c06189c
FelSiq <felipe.siqueira@usp.br>
2018-05-29 10:59:52 -0300

    Updated SearchButton with Link and Routing facilities.

--- frontend/src/SearchButton/SearchButton.js
+++ frontend/src/SearchButton/SearchButton.js
@@ -7,32 +7,40 @@ import FileUpload from '@material-ui/icons/FileUpload';
 import KeyboardVoice from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
 import Save from '@material-ui/icons/Save';
+import { Link } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 const styles = theme => ({
-  rightIcon: {
-    marginLeft: theme.spacing.unit,
-  },
+	rightIcon: {
+		marginLeft: theme.spacing.unit,
+	},
 });
 
 function IconLabelButtons(props) {
-  const { classes } = props;
-  return (
-    <div style={{display:'inline-block', padding:'0px 30px'}}>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet" />
-      <Button 
-        size="large"
-	className={classes.button} 
-	variant="raised"
-	color="primary">
-        <Icon className={classes.rightIcon}>send</Icon>
-      </Button>
-    </div>
-  );
+	const { classes } = props;
+	return (
+		<div style={{display:'inline-block', padding:'0px 30px'}}>
+			<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+				rel="stylesheet" />
+
+
+			<BrowserRouter>
+			<Link to={{pathname: props.searchLink}} className={classes.button}>
+				<Button 
+					size="large"
+					className={classes.button} 
+					variant="raised"
+					color="primary">
+					<Icon className={classes.rightIcon}>send</Icon>
+				</Button>
+			</Link>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 IconLabelButtons.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(IconLabelButtons);
