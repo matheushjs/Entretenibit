c413f38bc4f2bbcfa6e6f1263899ad425556d1a6
FelSiq <felipe.siqueira@usp.br>
2018-05-31 12:47:42 -0300

    Added base code for Header component.

---
+++ frontend/src/Header/Header.js
@@ -0,0 +1,74 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function SimpleAppBar(props) {
+	const { classes } = props;
+	return (
+		<div className={classes.root}>
+			<AppBar 
+				position="static" 
+				color="default">
+
+				<Toolbar>
+
+					<Link 
+						to="/home" >
+						<Button 
+							variant="raised" 
+							onClick={ props.homeButton } 
+							className={ classes.menuButton }> 
+							Home
+						</Button>
+					</Link>
+
+					<Link
+						to="/aboutus" >
+						<Button
+							variant="raised" 
+							className={ classes.menuButton }> 
+							Sobre Nós
+						</Button>
+					</Link>
+
+					<p className={classes.flex} />
+
+					<Link
+						to="/signup" >
+						<Button 
+							variant="raised" 
+							color="primary" 
+							className={ classes.menuButton }> 
+							Cadastre-se!
+						</Button>
+					</Link>
+
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
+
+SimpleAppBar.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleAppBar);
+
