f0a6e9a9060bd5048214d5466da8124e6d7fe260
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-31 23:29:05 -0300

    Change big to small image

--- frontend/src/CardGrid/SingleCard.js
+++ frontend/src/CardGrid/SingleCard.js
@@ -5,6 +5,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -15,32 +18,41 @@ const styles = {
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '56.25%', // Determines the size of the image
   },
 };
+
+const image_mapping = {
+  "academic": "/path_to_academic_image",
+  "theater": "/path_to_theater_image",
+  "musical": "/path_to_musical_image"
+}
 
 function SingleCard(props) {
   const { classes } = props;
   return (
     <div>
       <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image="/card_image_placeholder.jpeg"
-          title="Placeholder image"
+        <CardHeader
+          avatar={
+            <Avatar
+              alt="Placeholder"
+              src="/card_image_placeholder.jpeg"
+              className={classes.avatar}
+            />
+          }
+          title="Evento Muito Legal"
+          subheader="28 de Julho"
         />
         <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            Hello World
-            </Typography>
           <Typography component="p">
-            This is some secondary text.
-            </Typography>
+            O Evento Muito Legal invade a cidade de São Carlos com toda a eloquência esperada de um grupo de música pop.
+          </Typography>
         </CardContent>
         <CardActions>
           <Button size="small" color="primary">
             See More
-            </Button>
+          </Button>
         </CardActions>
       </Card>
     </div>
