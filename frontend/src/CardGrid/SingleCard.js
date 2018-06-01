c3750e76e735fd6f4ae9fd6682892555faaf0472
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-06-01 01:01:43 -0300

    Cleanup

--- frontend/src/CardGrid/SingleCard.js
+++ frontend/src/CardGrid/SingleCard.js
@@ -4,13 +4,10 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import { Link } from "react-router-dom";
 
 const styles = {
