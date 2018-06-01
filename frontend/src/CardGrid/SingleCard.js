1f2bf899a93cec60bf6510a0ac4586125a16c2eb
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-06-01 01:06:40 -0300

    Fix indentation

--- frontend/src/CardGrid/SingleCard.js
+++ frontend/src/CardGrid/SingleCard.js
@@ -11,109 +11,109 @@ import Typography from '@material-ui/core/Typography';
 import { Link } from "react-router-dom";
 
 const styles = {
-  card: {
-    marginLeft: "10%",
-    marginRight: "10%",
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // Determines the size of the image
-  },
-  avatar: {
-    margin: 3,
-  }
+	card: {
+		marginLeft: "10%",
+		marginRight: "10%",
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', // Determines the size of the image
+	},
+	avatar: {
+		margin: 3,
+	}
 };
 
 const image_mapping = {
-  "academic": "/card_icon_academic.png",
-  "theater": "/card_icon_theater.png",
-  "musical": "/card_icon_trumpet.png",
-  "unknown": "/card_icon_box.png"
+	"academic": "/card_icon_academic.png",
+	"theater": "/card_icon_theater.png",
+	"musical": "/card_icon_trumpet.png",
+	"unknown": "/card_icon_box.png"
 }
 
 const credit_mapping = {
-  "academic": "Icon made by Freepik at www.flaticon.com",
-  "theater": "Icon made by Pixel Buddha at www.flaticon.com",
-  "musical": "Icon made by Freepik at www.flaticon.com",
-  "unknown": "Icon made by Good Ware at www.flaticon.com",
+	"academic": "Icon made by Freepik at www.flaticon.com",
+	"theater": "Icon made by Pixel Buddha at www.flaticon.com",
+	"musical": "Icon made by Freepik at www.flaticon.com",
+	"unknown": "Icon made by Good Ware at www.flaticon.com",
 }
 
 
 function SingleCard(props) {
-  const {
-    title,
-    date,
-    type,
-    description,
-    classes,
-    link
-  } = props;
-  
-  
-  return (
-    <div>
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar
-              alt={credit_mapping[type]}
-              src={image_mapping[type]}
-              className={classes.avatar}
-            />
-          }
-          title={
-            <Typography gutterBottom variant="headline" component="h2">
-              {title}
-            </Typography>
-          }
-          subheader={date}
-        />
-        <CardContent>
-          <Typography component="p">
-            {description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link 
-            to={link} >
-          <Button size="small" color="primary">
-            Ver Mais
-          </Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </div>
-  );
+	const {
+		title,
+		date,
+		type,
+		description,
+		classes,
+		link
+	} = props;
+
+
+	return (
+		<div>
+			<Card className={classes.card}>
+				<CardHeader
+					avatar={
+						<Avatar
+							alt={credit_mapping[type]}
+							src={image_mapping[type]}
+							className={classes.avatar}
+						/>
+					}
+					title={
+						<Typography gutterBottom variant="headline" component="h2">
+							{title}
+						</Typography>
+					}
+					subheader={date}
+				/>
+				<CardContent>
+					<Typography component="p">
+						{description}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Link
+						to={link} >
+						<Button size="small" color="primary">
+							Ver Mais
+					</Button>
+					</Link>
+				</CardActions>
+			</Card>
+		</div>
+	);
 }
 
 SingleCard.propTypes = {
-  /* Title of the card */
-  title: PropTypes.string.isRequired,
+	/* Title of the card */
+	title: PropTypes.string.isRequired,
 
-  /* Date of the event. For now only strings are allowed */
-  subheader: PropTypes.string.isRequired,
+	/* Date of the event. For now only strings are allowed */
+	subheader: PropTypes.string.isRequired,
 
-  /* 
-   * Type of the event that will be displayed in the card.
-   * This determines the image that will be displayed in the card.
-   */
-  type: PropTypes.oneOf(["academic", "musical", "theater", "unknown"]),
-  
-  /* Description of the event */
-  description: PropTypes.string,
+	/* 
+	 * Type of the event that will be displayed in the card.
+	 * This determines the image that will be displayed in the card.
+	 */
+	type: PropTypes.oneOf(["academic", "musical", "theater", "unknown"]),
 
-  /* Link for the event page */
-  link: PropTypes.string,
+	/* Description of the event */
+	description: PropTypes.string,
 
-  /* For use with withStyles() */
-  classes: PropTypes.object.isRequired,
+	/* Link for the event page */
+	link: PropTypes.string,
+
+	/* For use with withStyles() */
+	classes: PropTypes.object.isRequired,
 };
 
 
 SingleCard.defaultProps = {
-  type: "unknown",
-  description: "Description unavailable.",
-  link: "*"
+	type: "unknown",
+	description: "Description unavailable.",
+	link: "*"
 }
 
 export default withStyles(styles)(SingleCard);
