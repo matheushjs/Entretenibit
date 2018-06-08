ca495eb8223040a1c30e870f4cc4eaee4a98e79f
Marcello <marcello_1000@yahoo.com.br>
2018-06-08 12:45:15 -0300

    Get the data

--- frontend/src/api/server.js
+++ frontend/src/api/server.js
@@ -5,5 +5,5 @@ axios.defaults.baseURL = "http://localhost:5000/";
 export function getEventType(type) {
   const endpoint = encodeURI(`/eventsType?type=${type}`);
 
-  return axios.get(endpoint).then(data => console.log(data));
+  return axios.get(endpoint).then(res => res.data);
 }
