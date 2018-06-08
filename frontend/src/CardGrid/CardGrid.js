1157ee73ce6126afcfb2bc8134c6590481cfb6ac
Marcello <marcello_1000@yahoo.com.br>
2018-06-08 12:16:42 -0300

    Connect to server

--- frontend/src/CardGrid/CardGrid.js
+++ frontend/src/CardGrid/CardGrid.js
@@ -2,120 +2,58 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import SingleCard from "./SingleCard";
 
-const bigTextTest=
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste"+
-	"texto grande para teste";
+import * as api from "../api/server";
 
-const example = [
-	[
-		{
-			title: "Workshop 1",
-			date: "2 de Janeiro",
-			type: "academic",
-			description: "Um grande workshop"+bigTextTest,
-			link: "https://www.lollapaloozabr.com/",
-		},
-		{
-			title: "Teatro 1",
-			date: "2 de Janeiro",
-			type: "theater",
-			description: "Um pequeno teatro"+bigTextTest,
-			link: "http://oasiseventossc.com.br/",
-		},
-	],
-	[
-		{
-			title: "Orquestra 1",
-			date: "2 de Janeiro",
-			type: "musical",
-			description: "OSUSP apresenta.",
-			link: "http://caipyra.python.org.br/",
-		},
-		{
-			title: "Pré-Estreia dum filmão",
-			date: "5 de Janeiro",
-			type: "unknown",
-			description: "Nada a declarar.",
-			link: "http://randomeventlink.web/info",
-		},
-	],
-	[
-		{
-			title: "Cansei de inventar títulos",
-			date: "6 de Janeiro",
-			type: "unknown",
-			description: "Bleh.",
-			link: "http://randomeventlink.web/info",
-		},
-	],
-];
+class CardGrid extends React.Component {
+  state = {
+    cards: []
+  };
 
-class CardGrid extends React.Component { //cars section (the third one)
-	render() {
+  componentWillMount() {
+    // const { type } = this.props;
+    const some = api.getEventType("escolar");
+    console.log(some);
+  }
 
-		return (
-			<Grid container spacing={24}>
-				{
-					example.map((value, index) => (
-						<Grid 
-							item xs={12} 
-							key={-index}>
+  render() {
+    const { cards } = this.state;
 
-							<Grid container justify="center" spacing={24}>
-								{
-									value[0] ?
-										<Grid item xs={6} key={index * 2 + 0}>
-											<SingleCard
-												cardAlign="top left"
-												title={value[0].title}
-												date={value[0].date}
-												type={value[0].type}
-												description={value[0].description}
-												link={value[0].link}
-											/>
-										</Grid>
-										: null
-								}
+    return (
+      <Grid container spacing={24}>
+        {cards.map((value, index) => (
+          <Grid item xs={12} key={-index}>
+            <Grid container justify="center" spacing={24}>
+              {value[0] ? (
+                <Grid item xs={6} key={index * 2 + 0}>
+                  <SingleCard
+                    cardAlign="top left"
+                    title={value[0].title}
+                    date={value[0].date}
+                    type={value[0].type}
+                    description={value[0].description}
+                    link={value[0].link}
+                  />
+                </Grid>
+              ) : null}
 
-								{
-									value[1] ?
-										<Grid item xs={6} key={index * 2 + 1}>
-											<SingleCard
-												cardAlign="top center"
-												title={value[1].title}
-												date={value[1].date}
-												type={value[1].type}
-												description={value[1].description}
-												link={value[1].link}
-											/>
-										</Grid>
-										: null
-								}
-							</Grid>
-						</Grid>
-					))
-				}
-			</Grid>
-		);
-	}
+              {value[1] ? (
+                <Grid item xs={6} key={index * 2 + 1}>
+                  <SingleCard
+                    cardAlign="top center"
+                    title={value[1].title}
+                    date={value[1].date}
+                    type={value[1].type}
+                    description={value[1].description}
+                    link={value[1].link}
+                  />
+                </Grid>
+              ) : null}
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
 }
 
 export default CardGrid;
--- frontend/src/api/server.js
+++ frontend/src/api/server.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.REACT_APP_API_URL || "https://localhost:5000";
+axios.defaults.baseURL = "http://localhost:5000/";
 
 export function getEventType(type) {
   const endpoint = encodeURI(`/eventsType?type=${type}`);
 
-  return axios
-    .get(endpoint)
-    .catch(err => console.log("Get Events by Type ERROR", error));
+  return axios.get(endpoint).then(data => console.log(data));
 }
