400e87bc310a907aba44da55431cf232e3c7e363
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-06-11 16:20:12 -0300

    Add test for front-end

--- test.bash
+++ test.bash
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Accumulates the number of errors
+error=0
+
 docker run --rm robot pytest
+error=$( expr $error + $? )
+
+docker run --rm --tty frontend yarn build | grep "Compiled successfully."
+error=$( expr $error + $? )
+
+# Export error in the correct variable
+exit $error
