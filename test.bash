5a845e64159fb57d66abc974c32bb804fab496a2
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-06-11 19:09:38 -0300

    Make test.bash more informative

--- test.bash
+++ test.bash
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Accumulates the number of errors
-error=0
-
 docker run --rm robot pytest
-error=$(( $error + $? ))
+robot_error=$?
 
 docker run --rm --tty frontend yarn build | grep "Compiled successfully."
-error=$(( $error + $? ))
+frontend_error=$?
+
+echo
+
+# Print some messages for helping the user to find the problem
+if [ $robot_error == 0 ]; then
+	echo "Robot passed the tests.";
+else
+	echo "Robot HAS NOT passed the tests.";
+fi
+echo
+
+if [ $frontend_error == 0 ]; then
+	echo "Frontend passed the tests.";
+else
+	echo "Frontend HAS NOT passed the tests.";
+	echo "Please, verify if the React code is having compilation errors or warnings.";
+fi
+echo
 
 # Export error in the correct variable
-exit $error
+exit $(( $robot_error + $frontend_error ))
