791bbca522f0d0c1338cf2655c101bbacfcce3ca
TMTiagoT <tiagosmi@hotmail.com>
2018-06-14 23:06:04 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
               path="/(home|search)/"
               render={() => {
                 return (
-		          <SelectPage />
+                  <SelectPage />
                 );
               }}
             />
