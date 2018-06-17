c0db2e41ac6e4f6f9df28b8235ab99fd58a2b33f
TMTiagoT <tiagosmi@hotmail.com>
2018-06-16 21:31:54 -0300

    fixing some react warnings

--- frontend/src/SignUpPage/SignUpPage.js
+++ frontend/src/SignUpPage/SignUpPage.js
@@ -11,13 +11,15 @@ function SignUpPage(props) {
       <div style={{ marginBottom: "5vh" }}>
 
         <div
-          className="UserSection"
+          className="UserStringSection"
           style={{
             marginTop: "0vh",
             marginLeft: "5%",
-            marginRight: "5%"
+            marginRight: "5%", 
+            textAlign: "left" 
           }}
         >
+
           <Typography
             style={{
               fontSize: "2vw",
@@ -26,46 +28,60 @@ function SignUpPage(props) {
           >
             <b>Como deseja receber nossos emails?</b>
           </Typography>
-              
+
+        </div>
+        
+        <div
+          className="UserEmailSection"
+          style={{ marginTop: "3vh", marginLeft: "5%", marginRight: "5%", textAlign: "left" }} 
+        >
+
           <p style={{
               fontSize: "1.5vw",
-              textAlign: "left"
+              textAlign: "left",
+              display: "inline"
             }}
           >Email: &emsp;
-          
-	        <SearchBar
-              onChange={props.handleInputChange}
-              onEnter={null}
-              getValue={props.email}
-              textStyle={{
-                margin: "auto",
-                width: "50vw"
-              }}
-              placeHolder={"Digite seu email"}
-              getName={"email"}
-            />
           </p>
           
+	      <SearchBar
+            onChange={props.handleInputChange}
+            onEnter={null}
+            getValue={props.email}
+            textStyle={{
+              margin: "auto",
+              width: "50vw"
+            }}
+            placeHolder={"Digite seu email"}
+            getName={"email"}
+          />
+
+        </div>
+        
+        <div
+          className="UserNameSection"
+          style={{ marginTop: "3vh", marginLeft: "5%", marginRight: "5%", textAlign: "left" }} 
+        >
+
           <p style={{
               fontSize: "1.5vw",
-              textAlign: "left"
+              textAlign: "left",
+              display: "inline"
             }}
           >Nome: &emsp;
-          
-            <SearchBar
-              onChange={props.handleInputChange}
-              onEnter={null}
-              getValue={props.name}
-              textStyle={{
-                margin: "auto",
-                width: "50vw"
-              }}
-              placeHolder={"Digite seu nome"}
-              getName={"name"}
-            />
           </p>
           
-          
+          <SearchBar
+            onChange={props.handleInputChange}
+            onEnter={null}
+            getValue={props.name}
+            textStyle={{
+              margin: "auto",
+              width: "50vw"
+            }}
+            placeHolder={"Digite seu nome"}
+            getName={"name"}
+          />
           
         </div>
 
@@ -84,27 +100,34 @@ function SignUpPage(props) {
 
         <div
           className="KeyWordsSection"
-          style={{ marginTop: "3vh", marginLeft: "5%", marginRight: "5%" }} >
+          style={{ marginTop: "3vh", marginLeft: "5%", marginRight: "5%", textAlign: "left" }} >
           
           <p style={{
               fontSize: "1.5vw",
-              textAlign: "left"
+              textAlign: "left",
+              display: "inline"
             }}
           >Busca: &emsp;
-          
-	        <SearchBar
-              onChange={props.handleInputChange}
-              onEnter={null}
-              getValue={props.searchString}
-              textStyle={{
-                margin: "auto",
-                width: "50vw"
-              }}
-              placeHolder={"O que você procura?"}
-              getName={"searchString"}
-            />
           </p>
+	        
+          <SearchBar
+            onChange={props.handleInputChange}
+            onEnter={null}
+            getValue={props.searchString}
+            textStyle={{
+              margin: "auto",
+              width: "50vw"
+            }}
+            placeHolder={"O que você procura?"}
+            getName={"searchString"}
+          />
 
+        </div>
+
+        <div
+          className="SignUpButtonSection"
+          style={{ marginTop: "3vh", marginLeft: "5%", marginRight: "5%"}} >
+          
           <LabelButtons 
             onClick={null}
             getText={"Cadastrar-se"}/>
